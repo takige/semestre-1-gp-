f3d004bbcf66cadcbc376b4277c24c51926eccab
takige <takibeskri38@gmail.com>
2026-01-29 02:17:46 +0100

    Update s1.js

--- s1.js
+++ s1.js
@@ -1,127 +1,43 @@
+function val(id) {
+  return Number(document.getElementById(id).value) || 0;
+}
+
+function setVal(id, value) {
+  document.getElementById(id).value = value.toFixed(2);
+}
+
 function calcu() {
 
-    var an1ntd = Number(document.getElementById('an1ntd').value) ;
-  
-   
-  
-    var an1ctrl = Number(document.getElementById('an1ctrl').value);
-  
-    
-  
-   
-  
-    var an1tt = document.getElementById('an1tt').value =  Number((an1ntd)*0.4+(an1ctrl*0.6));
-  
-  
-  
-  
-  
-    var ann1ntd = Number(document.getElementById('ann1ntd').value) ;
-  
-  
-    var ann1ctrl = Number(document.getElementById('ann1ctrl').value);
-  
-  
+  let an1tt  = val('an1ntd')*0.4 + val('an1ctrl')*0.6;
+  setVal('an1tt', an1tt);
 
+  let ann1tt = val('ann1ntd')*0.4 + val('ann1ctrl')*0.6;
+  setVal('ann1tt', ann1tt);
 
+  let me1tt  = val('me1td')*0.4 + val('me1ctrl')*0.6;
+  setVal('me1tt', me1tt);
 
-    var ann1tt = document.getElementById('ann1tt').value = Number((ann1ntd)*0.4+(ann1ctrl*0.6));
-  
-  
-  
-  
-    var me1td = Number(document.getElementById('me1td').value) ;
+  let on1tt  = (val('on1ntd')*4 + val('on1ctrl')*6) / 10;
+  setVal('on1tt', on1tt);
 
+  let po1tt  = (val('po1ntp')*4 + val('po1ctrl')*6) / 10;
+  setVal('po1tt', po1tt);
 
-    var me1ctrl = Number(document.getElementById('me1ctrl').value);
+  let hse1tt = val('hse1ctrl');
+  setVal('hse1tt', hse1tt);
 
-    
-    
+  let el1tt  = val('el1ctrl');
+  setVal('el1tt', el1tt);
 
+  let en1tt  = val('en1ctrl');
+  setVal('en1tt', en1tt);
 
-    var me1tt = document.getElementById('me1tt').value = Number((me1td)*0.4+(me1ctrl*0.6));
-  
-    
-  
-  
-  
-  
-  
-    var on1ntd = Number(document.getElementById('on1ntd').value) ;
-  
-     
-  
-    var on1ctrl = Number(document.getElementById('on1ctrl').value);
+  let de1tt  = val('de1tp');
+  setVal('de1tt', de1tt);
 
-  
-    var on1tt = document.getElementById('on1tt').value = Number(((on1ntd)*4+(on1ctrl*6))/10);
-  
-  
-  
-  
-  
-    var po1ntp = Number(document.getElementById('po1ntp').value);
-  
-    var po1ctrl = Number(document.getElementById('po1ctrl').value);
-  
-      
+  let total =
+    (3*an1tt + 2*ann1tt + 3*me1tt + 3*on1tt +
+     2*po1tt + hse1tt + el1tt + en1tt + de1tt) / 17;
 
-  
-    var po1tt = document.getElementById('po1tt').value = Number((((po1ntp)*4)+((po1ctrl)*6))/10);
-  
-  
-  
-  
-  
-  
-    
-    
-    var hse1ctrl = Number(document.getElementById('hse1ctrl').value);
-
-    var hse1tt = document.getElementById('hse1tt').value = Number((hse1ctrl)*1);
-  
-  
-  
-  
-  
-  
-     
-      var el1ctrl = Number(document.getElementById('el1ctrl').value);
-
-    var el1tt = document.getElementById('el1tt').value = Number((el1ctrl*1));
-  
-  
-  
-  
-  
-        
-
-        
-
-        var en1ctrl = Number(document.getElementById('en1ctrl').value);
-
-
-        var en1tt = document.getElementById('en1tt').value = Number((en1ctrl)*1);
-
-
-        
-
-
-
-
-
-
-        var de1tp = Number(document.getElementById('de1tp').value) ;
-
-        
-
-
-        var de1tt = document.getElementById('de1tt').value = Number((de1tp)*1);
-  
-
-
-        document.getElementById("tt").innerHTML = ((3*an1tt)+(2*ann1tt)+(3*me1tt)+(3*on1tt)+(2*po1tt)+(1*hse1tt)+(1*el1tt)+(1*en1tt)+(1*de1tt))/17;
-
-
-
-  }
+  document.getElementById("tt").innerHTML = total.toFixed(2);
+}
